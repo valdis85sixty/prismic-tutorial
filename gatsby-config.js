e52399747696685c75bc3c59f421b4b326a8054c
Valdis Locs <valdis.locs@digitaloperative.com>
2023-06-05 16:06:47 +0300

    - Add github Actions Workflow - Add gatsby-source-prismic and other dependencies - Set homepage(index.js) to fetch data from Prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,23 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `${process.env.PRISMIC_REPO_NAME}`,
+        // accessToken: `${process.env.PRISMIC_ACCESS_TOKEN}`,
+        customTypesApiToken: `${process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN}`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
-import * as React from "react"
+import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Index = ({ data }) => {
+  if (!data) return null
+  const home = data.prismicHomepage
+
+  return (
+    <React.Fragment>
+      <h1>{home.data.title.text}</h1>
+      <div dangerouslySetInnerHTML={{ __html: home.data.content.html }} />
+    </React.Fragment>
+  )
 }
+
+export default Index
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    prismicHomepage {
+      data {
+        title {
+          text
+        }
+        content {
+          html
+        }
+      }
+    }
+  }
+`
